e45561b58dc04b590c3dc0e38a2278d100972665
Raquel Brito <raquelbrito662@gmail.com>
2025-01-25 19:37:23 -0300

    feat: create fallbacks for store

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -1,10 +1,15 @@
 class ExchangeService
   def initialize
     @exchange_client = ExchangeClient.new
+    @exchange_cache = Cache.new
   end
 
   def self.perform
-    @exchange_client.exchange_rate
+    exchange_rate = @exchange_client.exchange_rate
+    rate = exchange_rate["rate"]
+    @exchange_cache.set("exchange_rate", rate.to_s, 60)
+
+    exchange_rate
     rescue => e
       Rails.logger.error("Error getting exchange rate: #{e.message}")
       fallback_exchange_rate
@@ -13,5 +18,12 @@ class ExchangeService
   private
 
   def fallback_exchange_rate
+    rate = @exchange_cache.get("exchange_rate")
+
+    if rate != null
+      rate.to_f
+    else
+      Rails.logger.error("Error getting exchange rate from cache")
+    end
   end
 end
